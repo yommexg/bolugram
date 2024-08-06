2edc8cd62d15043e25ae33258ca4f763f5be2c93
yommexg <hydrokidb@gmail.com>
2024-08-06 23:47:59 +0100

    update

--- components/UserListItem.tsx
+++ components/UserListItem.tsx
@@ -1,11 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import React from "react";
+import { useChatContext } from "stream-chat-expo";
+import { useAuth } from "@/providers/AuthProvider";
+import { router } from "expo-router";
 
 const UserListItem = ({ user }) => {
+  const { client } = useChatContext();
+
+  const { user: me } = useAuth();
+
+  const onPress = async () => {
+    if (me) {
+      const channel = client.channel("messaging", {
+        members: [me.id, user.id],
+      });
+
+      await channel.watch();
+
+      router.replace(`/channel\\${channel.cid}`);
+    }
+  };
+
   return (
-    <View style={{ padding: 15, backgroundColor: "white" }}>
+    <Pressable
+      onPress={onPress}
+      style={{ padding: 15, backgroundColor: "white" }}
+    >
       <Text style={{ fontWeight: "600" }}>{user.full_name}</Text>
-    </View>
+    </Pressable>
   );
 };
 
