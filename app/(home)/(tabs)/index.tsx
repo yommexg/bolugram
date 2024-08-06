ec16be44e11274990761f159337d71a70fa3befd
yommexg <hydrokidb@gmail.com>
2024-08-06 20:56:12 +0100

    update

--- app/(home)/(tabs)/index.tsx
+++ app/(home)/(tabs)/index.tsx
@@ -1,11 +1,17 @@
 import { ChannelList } from "stream-chat-expo";
 
 import { router } from "expo-router";
+import { useAuth } from "@/providers/AuthProvider";
 
 export default function MainTabScreen() {
+  const { user } = useAuth();
+
   return (
-    <ChannelList
-      onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
-    />
+    user && (
+      <ChannelList
+        filters={{ members: { $in: [user?.id] } }}
+        onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
+      />
+    )
   );
 }
